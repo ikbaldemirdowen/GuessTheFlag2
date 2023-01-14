c1b78054d870c564af1d1aeac4a841f6c7bf1671
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-01-13 16:48:18 -0800

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ikbal Demirdoven on 2023-01-13.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
